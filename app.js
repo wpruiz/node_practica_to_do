2b98e0d73497438974e1c5993f48b8ab5540f6be
wruiz <wilson.ruiz@eltransporte.org>
2020-02-28 23:07:11 -0500

    Ejercicio de To do

---
+++ app.js
@@ -0,0 +1,35 @@
+const argv = require('./config/yargs').argv
+const porHacer = require('./por-hacer/por-hacer')
+const colors = require('colors')
+
+let comando = argv._[0]
+
+switch (comando) {
+  case 'crear':
+    // console.log('Crear por hacer');
+    let tarea =  porHacer.crear(argv.descripcion)
+    console.log(tarea)
+  break;
+  case 'listar':
+    // console.log('Mostrar todas las tareas por hacer');
+    let listado = porHacer.getListado();
+    for(let tarea of listado){
+      console.log('=====Por Hacer===='.green)
+      console.log(tarea.descripcion)
+      console.log('Estado: ', tarea.completado)
+      console.log('=================='.green)
+    }
+  break;
+  case 'actualizar':
+    let actualizar = porHacer.actualizar(argv.descripcion, argv.completado)
+    console.log(actualizar)
+    // console.log('Actualiza una tarea por hacer');
+  break;
+  case 'borrar':
+    let borrado = porHacer.borrar(argv.descripcion)
+    console.log(borrado)
+    // console.log('Actualiza una tarea por hacer');
+  break;
+  default:
+    console.log('Comando no reconocido');
+}
